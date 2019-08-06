4459e73cab0e286bcc685884c655b25cd1402af4
inguuu <dlsrb8949@gmail.com>
2019-08-06 11:40:51 +0900

    commit findAll

--- study3/src/main/java/com/example/study3/service/BoardService.java
+++ study3/src/main/java/com/example/study3/service/BoardService.java
@@ -1,4 +1,16 @@
 package com.example.study3.service;
 
+import com.example.study3.model.Board;
+import com.example.study3.model.DefaultRes;
+
+import java.util.*;
+
 public interface BoardService {
+
+    DefaultRes<List<Board>> findAll();
+
+    DefaultRes<Board> findIdx(int boardIdx);
+
+    DefaultRes insert(Board board);
+
 }
--- study3/src/main/java/com/example/study3/service/impl/BoardServiceImpl.java
+++ study3/src/main/java/com/example/study3/service/impl/BoardServiceImpl.java
@@ -1,4 +1,36 @@
 package com.example.study3.service.impl;
 
-public class BoardServiceImpl {
+import com.example.study3.model.Board;
+import com.example.study3.model.DefaultRes;
+import com.example.study3.service.BoardService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    private static List<Board> boardList = new ArrayList<>();
+
+    @Override
+    public DefaultRes<List<Board>> findAll() {
+        if(boardList.isEmpty()){
+            return new DefaultRes<>(HttpStatus.NO_CONTENT,"유저 정보가 없습니다.");
+        }else{
+            return new DefaultRes<List<Board>>(HttpStatus.OK.value(), "조회성공", boardList);
+        }
+    }
+
+    @Override
+    public DefaultRes<Board> findIdx(int boardIdx) {
+        return null;
+    }
+
+    @Override
+    public DefaultRes insert(Board board) {
+        return null;
+    }
 }
